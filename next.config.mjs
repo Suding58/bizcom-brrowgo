f2467ade695f371db5a1ad17e5001f8c701653f1
Suding58 <62562244+Suding58@users.noreply.github.com>
2024-10-22 13:19:00 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,9 @@ const nextConfig = {
     ],
   },
   env: {
-    TELEGRAM_BOT_TOKEN: "7282599182:AAGDRiq26S9M2XRF3cNPJsHUDE1C8rfOla0",
-    TELEGRAM_CHAT_ID: "-1002373716259",
-    LINE_NOTIFY_TOKEN: "erBJ9GYViE1EeH2i5pRrOMQZbV4BEVHDqU52PRwiT23",
+    TELEGRAM_BOT_TOKEN: "",
+    TELEGRAM_CHAT_ID: "",
+    LINE_NOTIFY_TOKEN: "",
   },
 };
 export default nextConfig;
